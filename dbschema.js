4a85475da21b186508d6fd269ea19533b661a4db
Jeferson Eiji <jeferson.eiji@icloud.com>
2020-07-31 13:21:21 -0300

    emulator with error

---
+++ dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+    posts: [
+        {
+            userHandle: 'user',
+            body: 'some random text',
+            createdAt: 'xxx xxx xx',
+            likeCount: 5,
+            commentCount: 2
+        }
+    ]
+}
